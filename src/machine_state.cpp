0954468d39ce8686dacd835b7e4ce0ff55d9b248
elmerucr <elmerucr@gmail.com>
2019-09-23 23:30:05 +0200

    simplify machine_state

--- src/machine_state.cpp
+++ src/machine_state.cpp
@@ -28,23 +28,14 @@ void E64::machine_state_switch_mode()
 
 void E64::machine_state_switch_to_debug(void)
 {
-    E64::sdl2_stop_audio();
     machine_mode_current = E64_DEBUG;
+    E64::sdl2_stop_audio();
     E64::sdl2_update_title();
 
-    // machine halted, dump cpu status (should be switching depending on if bar is visible or not)
     debug_console_putchar('\n');
-    //c256_debug_command_dump_cpu_status();
     debug_console_put_prompt();
     debug_status_bar_clear();
     debug_status_bar_refresh();
-
-    // the next three function calls are necessary to force a screen update when entering debug.
-    // in debug mode, a screen refresh occurs only when an event has happened
-    // and as such, debug mode stays 'dark' at first when these functions wouldn't be called
-    debug_console_blit_to_debug_screen();
-    debug_screen_update();
-    E64::sdl2_update_screen();
 }
 
 void E64::machine_state_switch_to_running(void)
